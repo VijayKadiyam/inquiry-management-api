01b1adcb5a95aa54102dd086296b15c1a8a03ade
VijayKadiyam <freelancervijayk@gmail.com>
2018-04-17 14:58:22 +0530

    Deploying to aws 1.0.4

--- app/Http/Middleware/CORSMiddleware.php
+++ app/Http/Middleware/CORSMiddleware.php
@@ -16,7 +16,7 @@ class CORSMiddleware
     public function handle($request, Closure $next)
     {
         return $next($request)
-          ->header('Access-Control-Allow-Origin', 'http://localhost:8081')
+          ->header('Access-Control-Allow-Origin', '*')
           ->header('Access-Control-Allow-Methods', '*')
           ->header('Access-Control-Allow-Headers', '*');
     }
