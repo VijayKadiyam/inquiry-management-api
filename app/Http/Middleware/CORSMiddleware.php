ce15868f227e030da71ec392c8d138296159a6a9
VijayKadiyam <freelancervijayk@gmail.com>
2018-04-17 15:39:35 +0530

    Deploying to aws 1.0.7

--- app/Http/Middleware/CORSMiddleware.php
+++ app/Http/Middleware/CORSMiddleware.php
@@ -15,15 +15,10 @@ class CORSMiddleware
      */
     public function handle($request, Closure $next)
     {
-      return $next($request)
-        ->header("Access-Control-Expose-Headers", "Access-Control-*")
-        ->header("Access-Control-Allow-Headers", "Access-Control-*, Origin, X-Requested-With, Content-Type, Accept")
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD')
-        ->header('Access-Control-Allow-Origin', '*');
-        
-        // ->header('Allow', 'GET, POST, PUT, DELETE, OPTIONS, HEAD');
-        //   ->header('Access-Control-Allow-Origin', '*')
-        //   ->header('Access-Control-Allow-Methods', '*')
-        //   ->header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+      return $next($request) 
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', '*'); 
+
     }
 }
