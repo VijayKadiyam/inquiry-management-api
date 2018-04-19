131ea3dcc79564b04d5eb2700db13e137f9c518d
VijayKadiyam <freelancervijayk@gmail.com>
2018-04-19 18:53:45 +0530

    Data fetching issue 1.0.4

--- app/Http/Middleware/CORSMiddleware.php
+++ app/Http/Middleware/CORSMiddleware.php
@@ -18,7 +18,8 @@ class CORSMiddleware
       return $next($request) 
         ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', '*'); 
+        ->header('Access-Control-Allow-Headers', '*')
+        ->header('Access-Control-Expose-Headers', '*'); 
 
     }
 }
