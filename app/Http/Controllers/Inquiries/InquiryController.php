69016cfa4d1ce6931e9d18c55be87bb01c201fa3
VijayKadiyam <freelancervijayk@gmail.com>
2018-04-19 18:27:12 +0530

    Data fetching issue 1.0.2

--- app/Http/Controllers/Inquiries/InquiryController.php
+++ app/Http/Controllers/Inquiries/InquiryController.php
@@ -21,6 +21,10 @@ class InquiryController extends Controller
    */
   public function index()
   {  
+    return response()->json([
+      'data'  =>  request()->header('company_id')
+    ]);
+
     $company = Company::where('id', '=', request()->header('company_id'))->first();
     if($company)
       $inquiries = $company->inquiries;
